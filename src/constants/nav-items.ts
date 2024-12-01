e5ee09c9670b82a836f2e1ee4888947d23febc29
kohdc1723 <kohdc1723@gmail.com>
2024-11-30 16:39:12 -0800

    preinstall legacy-peer-deps

--- src/constants/nav-items.ts
+++ src/constants/nav-items.ts
@@ -1,9 +1,7 @@
 import { IconType } from "react-icons/lib";
 import { CgHomeAlt, CgViewList, CgFile, CgFileDocument } from "react-icons/cg";
-import { NavValue } from "@/types/nav-item";
 
 interface NavItem {
-  value: NavValue;
   title: string;
   href: string;
   icon: IconType;
@@ -11,25 +9,21 @@ interface NavItem {
 
 export const navItems: NavItem[] = [
   {
-    value: "home",
     title: "Home",
     href: "/",
     icon: CgHomeAlt
   },
   {
-    value: "resume",
     title: "Resume",
     href: "/resume",
     icon: CgFile
   },
   {
-    value: "cover-letter",
     title: "Cover Letter",
     href: "/cover-letter",
     icon: CgFileDocument
   },
   {
-    value: "application",
     title: "Application",
     href: "/application",
     icon: CgViewList
